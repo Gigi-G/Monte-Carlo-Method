9e75b3c82d42f9b1bccf66e7c8c9f56726848832
Gigi-G <seminara.luigi@gmail.com>
2021-03-24 16:01:02 +0100

    feat&fix: Chart with error bars

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions } from 'chart.js';
+import { ChartDataSets, Chart, ChartData } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
+import * as pluginErrorBars from 'chartjs-plugin-error-bars'
+
+Chart.plugins.register(pluginErrorBars)
 
 @Component({
   selector: 'app-line-chart',
@@ -9,15 +12,13 @@ import { Color, Label } from 'ng2-charts';
 })
 export class LineChartComponent implements OnInit {
 
+  constructor() { }
+
   lineChartData: ChartDataSets[] = [
     { data: [], label: 'Estimated area'}
   ];
 
   lineChartLabels: Label[] = ['100', '1000', '100000', '1000000', '10000000', '100000000', '1000000000'];
-
-  lineChartOptions = {
-    responsive: true,
-  };
 
   lineChartColors: Color[] = [
     {
@@ -26,13 +27,46 @@ export class LineChartComponent implements OnInit {
     }
   ];
 
-  lineChartLegend = true;
-  lineChartPlugins = [];
   lineChartType = 'line';
 
-  constructor() { }
+  lineChartErrorBars = [];
+
+  private createLineChartDataset(): any {
+    let lineChartDataset = {
+      labels: ['100', '1000', '100000', '1000000', '10000000', '100000000', '1000000000'],
+      datasets: [],
+    };
+    let datasets = {};
+    for(let i = 0; i < this.lineChartData.length; ++i) {
+      datasets = {
+        label: this.lineChartData[i].label,
+        data: this.lineChartData[i].data,
+        borderColor: this.lineChartColors[i].borderColor,
+        backgroundColor: this.lineChartColors[i].backgroundColor,
+        errorBars: this.lineChartErrorBars[i]
+      };
+      lineChartDataset.datasets.push(datasets);
+    }
+    return lineChartDataset;
+  }
+
+  public updateView(): void {
+    let ctx = (document.getElementById("canvas") as HTMLCanvasElement).getContext("2d");
+    new Chart(ctx, {
+      type: this.lineChartType,
+      data: this.createLineChartDataset(),
+      options: {
+        responsive: true,
+        legend: {
+          position: 'top',
+        },
+        plugins: [pluginErrorBars]
+      },
+    });
+  }
 
   ngOnInit(): void {
+    this.updateView();
   }
 
 }
